a635ae5009facddbdfedb3ad1532b954f7f0ca5b
tohousanae <122202405+tohousanae@users.noreply.github.com>
2024-07-02 02:04:17 +0800

    編寫dapper用的連線字串，研究非同步查詢

--- am3buger/Repository/UserRepository.cs
+++ am3buger/Repository/UserRepository.cs
@@ -2,5 +2,6 @@
 {
     public class UserRepository
     {
+        private readonly string _connectString = @"(localdb)\\ProjectModels;Initial Catalog=am3burger;TrustServerCertificate=True;Integrated Security=true";
     }
 }
