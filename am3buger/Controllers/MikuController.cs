fb66098bd04681d43f7c7970f405d10c806b5717
tohousanae <122202405+tohousanae@users.noreply.github.com>
2025-04-07 16:34:27 +0800

    新增miku控制器，未來作為首頁與v家情報相關頁面python爬蟲推播用

---
+++ am3buger/Controllers/MikuController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+System.Diagnostics.Process
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace am3burger.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MikuController : ControllerBase
+    {
+        // GET: api/<MikuController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<MikuController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MikuController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MikuController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MikuController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
