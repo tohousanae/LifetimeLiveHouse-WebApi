73b57ecc1924d0617296725f6a71144cc6733463
tohousanae <122202405+tohousanae@users.noreply.github.com>
2025-04-08 16:35:04 +0800

    gpt給我的using代碼有錯，先暫時刪掉

--- am3buger/Controllers/MikuController.cs
+++ am3buger/Controllers/MikuController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics.Process
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
