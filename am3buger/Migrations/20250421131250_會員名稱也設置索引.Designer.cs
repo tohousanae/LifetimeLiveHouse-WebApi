494bf41ac73cb678801bb9361ccbe1769729d05f
tohousanae <122202405+tohousanae@users.noreply.github.com>
2025-04-21 21:13:12 +0800

    會員名稱也設置索引

--- am3buger/Migrations/20250421131250_會員名稱也設置索引.Designer.cs
+++ am3buger/Migrations/20250421131250_會員名稱也設置索引.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using am3burger.Models;
 
@@ -11,9 +12,11 @@ using am3burger.Models;
 namespace am3burger.Migrations
 {
     [DbContext(typeof(Am3burgerContext))]
-    partial class Am3burgerContextModelSnapshot : ModelSnapshot
+    [Migration("20250421131250_會員名稱也設置索引")]
+    partial class 會員名稱也設置索引
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- am3buger/Models/User.cs
+++ am3buger/Models/User.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 namespace am3burger.Models
 {
     // 不常變動的資料增加索引，提高查詢速度
+    [Index(nameof(Name), IsUnique = true, Name = "IX_User_Name")]
     [Index(nameof(Email), IsUnique = true, Name = "IX_User_Email")]
     [Index(nameof(PhoneNumber), IsUnique = true, Name = "IX_User_PhoneNumber")]
     [Index(nameof(Sex), IsUnique = true, Name = "IX_User_Sex")]
