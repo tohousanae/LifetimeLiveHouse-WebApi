8aab66f074260e8d864fb052a2422c9cf91fd1e7
tohousanae <122202405+tohousanae@users.noreply.github.com>
2025-04-21 20:35:35 +0800

    提交user索引鍵資料庫變更

--- am3buger/Migrations/20250421122744_user資料表部分資料加索引鍵.Designer.cs
+++ am3buger/Migrations/20250421122744_user資料表部分資料加索引鍵.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using am3burger.Models;
 
@@ -11,9 +12,11 @@ using am3burger.Models;
 namespace am3burger.Migrations
 {
     [DbContext(typeof(Am3burgerContext))]
-    partial class Am3burgerContextModelSnapshot : ModelSnapshot
+    [Migration("20250421122744_user資料表部分資料加索引鍵")]
+    partial class user資料表部分資料加索引鍵
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
