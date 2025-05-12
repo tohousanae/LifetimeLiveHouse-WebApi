08f16c0ec7f541fd04831edb2387ad531d90edfc
tohousanae <122202405+tohousanae@users.noreply.github.com>
2025-05-12 15:38:01 +0800

    上傳訂單資料表

---
+++ am3buger/Migrations/20250512071127_修改訂單資料表.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace am3burger.Migrations
+{
+    /// <inheritdoc />
+    public partial class 修改訂單資料表 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoName",
+                table: "OrderForm");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "OrderForm");
+
+            migrationBuilder.DropColumn(
+                name: "ProductName",
+                table: "OrderForm");
+
+            migrationBuilder.DropColumn(
+                name: "Store",
+                table: "OrderForm");
+
+            migrationBuilder.DropColumn(
+                name: "Total_price",
+                table: "OrderForm");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoName",
+                table: "OrderForm",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Price",
+                table: "OrderForm",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductName",
+                table: "OrderForm",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Store",
+                table: "OrderForm",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Total_price",
+                table: "OrderForm",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
--- am3buger/Models/OrderForm.cs
+++ am3buger/Models/OrderForm.cs
@@ -13,24 +13,6 @@ namespace am3burger.Models
         [Display(Name = "訂單時間")]
         public DateTime OrderTime { get; set; }
 
-        [Display(Name = "店家名稱")]
-        [StringLength(50)]
-        public string? Store { get; set; }
-
-        [Display(Name = "商品名稱")]
-        [StringLength(50)]
-        public string? ProductName { get; set; }
-
-        [Display(Name = "商品圖片名稱")]
-        [StringLength(50)]
-        public string? PhotoName { get; set; }
-
-        [Display(Name = "商品售價")]
-        public int? Price { get; set; }
-
-        [Display(Name = "總計")]
-        public int? Total_price { get; set; }
-
         [Display(Name = "付款方式")]
         public string? Payment { get; set; }
     }
