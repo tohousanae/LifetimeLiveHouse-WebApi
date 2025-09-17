a2f6183ff0d29d5df1526885018564261238581d
tohousanae <122202405+tohousanae@users.noreply.github.com>
2025-09-17 10:22:34 +0800

    Update MemberAccount.cs

--- HatsuneMIkuShop.Models/MemberAccount.cs
+++ HatsuneMIkuShop.Models/MemberAccount.cs
@@ -6,7 +6,7 @@ public partial class MemberAccount
 {
     [Key]
     [StringLength(30)]
-    public string Account { get; set; } = null!;
+    public string Email { get; set; } = null!;
 
     [StringLength(200)]
     public string Password { get; set; } = null!;
