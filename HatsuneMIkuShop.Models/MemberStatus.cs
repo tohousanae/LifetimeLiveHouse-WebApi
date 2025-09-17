becb207dc11e338e5845a801162fd189318e32d5
tohousanae <122202405+tohousanae@users.noreply.github.com>
2025-09-17 10:25:32 +0800

    Create MemberStatus.cs

---
+++ HatsuneMIkuShop.Models/MemberStatus.cs
@@ -0,0 +1,16 @@
+﻿using LifetimeLiveHouse.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public partial class MemberStatus
+{
+    [Key]
+    [StringLength(1)]
+    public string Email { get; set; } = null!;
+
+    [StringLength(200)]
+    public string Password { get; set; } = null!;
+
+    [ForeignKey("Member")]
+    public long MemberID { get; set; }
+}
