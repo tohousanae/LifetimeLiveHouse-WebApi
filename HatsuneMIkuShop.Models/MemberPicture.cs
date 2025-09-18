0066afb7d9d271af306631668ac576c4bda6e309
tohousanae <122202405+tohousanae@users.noreply.github.com>
2025-09-19 00:04:32 +0800

    新增會員圖庫資料模型

--- HatsuneMIkuShop.Models/MemberPicture.cs
+++ HatsuneMIkuShop.Models/MemberPicture.cs
@@ -1,11 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-public partial class MemberAccount
+public partial class MemberPicture
 {
     [Key]
     [StringLength(30)]
-    public string Email { get; set; } = null!;
+    public string Picture { get; set; } = null!;
 
     [StringLength(200)]
     public string Password { get; set; } = null!;
