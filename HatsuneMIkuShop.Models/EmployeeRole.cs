84582a1bc7ace8e5dca017a87489b685edff64e8
tohousanae <122202405+tohousanae@users.noreply.github.com>
2025-09-25 01:53:06 +0800

    上船變更

--- HatsuneMIkuShop.Models/EmployeeRole.cs
+++ HatsuneMIkuShop.Models/EmployeeRole.cs
@@ -16,5 +16,6 @@ public partial class EmployeeRole
     [StringLength(15)]
     public string RoleName { get; set; } = null!;
 
+    // 集合導覽是 .NET 集合類型的實例;也就是說，任何實作 ICollection<T>的類型。 集合包含相關實體類型的實例，其中可以有任意數目。 它們代表了一對多和多對多關係的“多”端。 
     public virtual ICollection<Employee> Employee { get; set; } = new List<Employee>();
 }
