318179c01805c283c67abd247a62ea2ca235e68b
tohousanae <122202405+tohousanae@users.noreply.github.com>
2025-10-19 20:24:02 +0800

    刪除資料庫查詢性能優化前得登入api註解

--- HatsuneMikuShopWebAPI/Services/Implementations/MemberLoginService.cs
+++ HatsuneMikuShopWebAPI/Services/Implementations/MemberLoginService.cs
@@ -18,47 +18,6 @@ namespace LifetimeLiveHouseWebAPI.Services.Implementations
             _context = context;
         }
 
-        //public async Task<ActionResult<string>> LoginAsync(LoginDTO loginDto, HttpContext httpContext)
-        //{
-        //    if (loginDto == null || string.IsNullOrEmpty(loginDto.Email) || string.IsNullOrEmpty(loginDto.Password))
-        //        return new UnauthorizedObjectResult("請輸入帳號和密碼");
-
-        //    var user = await _context.MemberAccount
-        //        .Include(u => u.Member)
-        //        .Include(u => u.Member.MemberVerificationStatus)
-        //        .FirstOrDefaultAsync(u => u.Email == loginDto.Email);
-
-        //    if (user == null || !BCrypt.Net.BCrypt.Verify(loginDto.Password, user.Password))
-        //        return new UnauthorizedObjectResult("帳號或密碼錯誤，請重新輸入");
-
-        //    if (user.Member.StatusCode == "1")
-        //        return new UnauthorizedObjectResult("該帳號已停權，請檢察您的電子郵件");
-
-        //    if (user.Member.MemberVerificationStatus.EmailVerificationStatus == false)
-        //    {
-        //        return new UnauthorizedObjectResult("未完成電子郵件驗證");
-        //    }
-        //    if (user.Member.MemberVerificationStatus.PhoneVerificationStatus == false)
-        //    {
-        //        return new UnauthorizedObjectResult("未完成手機號碼驗證");
-        //    }
-
-        //    //if (user.Member.)
-        //    var claims = new List<Claim>
-        //    {
-        //        new Claim(ClaimTypes.Actor, user.Email),
-        //        new Claim(ClaimTypes.Role, "Member"),
-        //        new Claim(ClaimTypes.Sid, user.MemberID.ToString()),
-        //        new Claim(ClaimTypes.Name, user.Member.Name)
-        //    };
-
-        //    var claimsIdentity = new ClaimsIdentity(claims, "MemberLogin");
-        //    var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
-
-        //    await httpContext.SignInAsync("MemberLogin", claimsPrincipal);
-
-        //    return new OkObjectResult("登入成功");
-        //}
         public async Task<ActionResult<string>> LoginAsync(LoginDTO loginDto, HttpContext httpContext)
         {
             if (loginDto == null || string.IsNullOrEmpty(loginDto.Email) || string.IsNullOrEmpty(loginDto.Password))
