86068d3542f29e9e5a1a9e30d7add0c2977bd556
tohousanae <122202405+tohousanae@users.noreply.github.com>
2026-08-18 23:16:27 +0800

    建立IMemberProfileService

---
+++ HatsuneMikuShopWebAPI/Modules/User/Interfaces/IMemberProfileService.cs
@@ -0,0 +1,11 @@
+﻿using LifetimeLiveHouseWebAPI.DTOs.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LifetimeLiveHouseWebAPI.Modules.User.Interfaces
+{
+    public interface IMemberProfileService
+    {
+        Task<ActionResult<object>> GetMemberProfileAsync(long memberId);
+        Task<ActionResult<string>> UpdateMemberProfileAsync(long memberId, MemberUpdateDTO dto);
+    }
+}
